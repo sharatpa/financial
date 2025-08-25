2453439adbe6ceb6402b6117e207262bb646ff60
Sharat <sharatpc@gmail.com>
2025-08-25 21:30:02 +0530

    Adding indicators script.

---
+++ trading_strats/indicators.py
@@ -0,0 +1,12 @@
+"""Class for technical analysis indicators."""
+
+
+class Indicators():
+    """Defines indicators."""
+
+    def __init__(self):
+        return None
+
+    def macd(self):
+        """Moving average convergence divergence."""
+        return None
