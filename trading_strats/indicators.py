3bab67828bd7b21e64c9f29739adc42fab18def7
Sharat <sharatpc@gmail.com>
2025-08-29 21:21:52 +0530

    Added macd to indicators.py.

--- trading_strats/indicators.py
+++ trading_strats/indicators.py
@@ -7,6 +7,13 @@ class Indicators():
     def __init__(self):
         return None
 
-    def macd(self):
-        """Moving average convergence divergence."""
-        return None
+    def macd(self, tag):
+        """Moving average convergence divergence. Difference between 12
+        period EMA and 26 period EMA.
+        Input: tag (df column) representing the close value.
+        Output: MACD."""
+        exp1 = tag.ewm(span=12, adjust=False).mean()
+        exp2 = tag.ewm(span=26, adjust=False).mean()
+        macd = exp1 - exp2
+        signal_line = macd.ewm(span=9, adjust=False).mean()
+        return macd, signal_line
